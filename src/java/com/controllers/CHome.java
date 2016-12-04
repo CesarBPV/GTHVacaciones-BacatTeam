a33681436940ea11c1f613ce75a2d23110d078e5
CESAR <CESAR@169.254.103.192>
2016-12-04 18:57:42 -0500

    some modifications

--- src/java/com/controllers/CHome.java
+++ src/java/com/controllers/CHome.java
@@ -7,7 +7,10 @@ package com.controllers;
 
 import com.dao.usuarioDAO;
 import com.interfaces.ImpUsuarioDao;
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -26,43 +29,55 @@ public class CHome {
     @RequestMapping("/menu")
     public String menu(HttpServletRequest request, HttpServletResponse response) {
         HttpSession sesion = request.getSession();
-        if (request.getSession().getAttribute("idusuario") == null) {
-            String username = request.getParameter("username");
-            String password = request.getParameter("password");
-            if (username != null && password != null) {
-                List<String> list = udao.validar(username, password);
-                System.out.println("idusuario:   "+ list.get(0));
-                if (list.get(0) != null) {
-                    sesion.setAttribute("idusuario", list.get(0));
-                    sesion.setAttribute("idtrabajador", list.get(1));
-                    return "modules";
-                }else{
-                    return "login";
+        try {
+            if (request.getSession().getAttribute("idusuario") == null) {
+                String username = request.getParameter("username");
+                String password = request.getParameter("password");
+                if (username != null && password != null) {
+                    List<String> list = udao.validar(username, password);
+                    System.out.println("idusuario:   " + list.get(0));
+                    if (list.get(0) != null) {
+                        sesion.setAttribute("idusuario", list.get(0));
+                        sesion.setAttribute("idtrabajador", list.get(1));
+                        return "modules";
+                    } else {
+                        response.sendRedirect("login");
+                    }
+                } else {
+                    response.sendRedirect("login");;
                 }
-            }else{
-                return "login";
+            } else {
+                System.out.println(request.getSession().getAttribute("idusuario"));
+                return "modules";
             }
-        } else {
-            System.out.println(request.getSession().getAttribute("idusuario"));
-            return "modules";
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex);
         }
+        return "modules";
     }
 
     @RequestMapping("/login")
     public String login(HttpServletRequest request, HttpServletResponse response) {
         HttpSession sesion = request.getSession();
-        String opc=request.getParameter("opc");
-        if("logout".equals(opc)){
+        String opc = request.getParameter("opc");
+        if ("logout".equals(opc)) {
             sesion.removeAttribute("idusuario");
+        } else {
+            try {
+                response.sendRedirect("index");
+            } catch (IOException ex) {
+                Logger.getLogger(CHome.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
         return "login";
     }
+
     @RequestMapping("/index")
     public String index(HttpServletRequest request, HttpServletResponse response) {
         HttpSession sesion = request.getSession();
-        if(sesion.getAttribute("idusuario")==null){
+        if (sesion.getAttribute("idusuario") == null) {
             return "login";
-        }else{
+        } else {
             return "index";
         }
     }
